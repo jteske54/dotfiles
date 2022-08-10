3375bf61e24cff2deccb887648e986583255c54f
Jacob Teske <teske.jacob@gmail.com>
2022-08-10 18:34:08 -0500

    added

--- setup.sh
+++ setup.sh
@@ -4,5 +4,14 @@
 SCRIPT=$(realpath "$0")
 SCRIPTPATH=`dirname "$SCRIPT"`
 
+echo "Starting setup..."
+rm ~/.bashrc
 ln -s "${SCRIPTPATH}/bash/.bashrc" ~/.bashrc
+echo ".bashrc complete"
+rm ~/.bash_logout
 ln -s "${SCRIPTPATH}/bash/.bash_logout" ~/.bash_logout
+echo ".bash_logout complete"
+
+
+
+echo "Setup complete!"
